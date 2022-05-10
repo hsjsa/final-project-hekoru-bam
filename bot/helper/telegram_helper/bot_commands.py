214849f716de0a1206b65a17506f5cb9a4624436
Your Name <you@example.com>
2022-05-10 08:46:44 +0530

    rkjh

---
+++ bot/helper/telegram_helper/bot_commands.py
@@ -0,0 +1,24 @@
+class _BotCommands:
+    def __init__(self):
+        self.StartCommand = 'start'
+        self.MirrorCommand = 'mirror'
+        self.UnzipMirrorCommand = 'unzip'
+        self.ZipMirrorCommand = 'zip'
+        self.CancelMirror = 'cancel'
+        self.CancelAllCommand = 'cancelall'
+        self.ListCommand = 'list'
+        self.StatusCommand = 'status'
+        self.AuthorizedUsersCommand = 'users'
+        self.AuthorizeCommand = 'auth'
+        self.UnAuthorizeCommand = 'unauth'
+        self.AddSudoCommand = 'add'
+        self.RmSudoCommand = 'rm'
+        self.PingCommand = 'ping'
+        self.RestartCommand = 'restart'
+        self.StatsCommand = 'stats'
+        self.HelpCommand = 'help'
+        self.LogCommand = 'log'
+        self.CloneCommand = 'clone'
+        self.DeleteCommand = 'del'
+
+BotCommands = _BotCommands()
